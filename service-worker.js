9c09fe4a58c9be3c96659c0007b381eb4d07d580
sebastianmehler-cpu <sebastian.mehler@gmx.de>
2026-08-12 11:41:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,5 @@
-const CACHE_NAME = "keller-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/app.js"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener("install", () => {
+    console.log("SW installiert");
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener("fetch", () => {});
